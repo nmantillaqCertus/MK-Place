f69860891f0d2be9fef62840279e673f1d180771
nmantillaqCertus <90099252+nmantillaqCertus@users.noreply.github.com>
2022-05-30 08:53:16 -0500

    updatev2

--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,34 @@
-/*window.onload = function(){
-    console.log("Hola  Mundo");
-};*/
 
 $(document).ready(function(){
-    //console.log( "todo listo para usar" );
     secciones();
-
-    $(".ig").on("click", function(){
-        window.location.href = "https://www.google.com.pe/";
-    });
 });
 
 
-//Declaracion de Variables
-let arrayPersonas = [];
-
-function guardar(){
-    let vNombre = $("#idNombre").val();
-    let vApellido = $("#idApellido").val();
-    let vEdad = $("#idEdad").val();
-    let vDireccion = $("#idDireccion").val();
-
-    arrayPersonas.push({
-        aNombre: vNombre
-    });
-}
 
 function secciones(){
-    $("#idSecUno").on("click", function(){
-        $("#idSeccion01").show();
-        $("#idSeccion02").hide();
-        $("#idSeccion03").hide();
+
+    /*Lading*/
+    $("#idSecLading").on("click", function(){
+        $("#idLanding").show();
+        $("#idSeccion01").hide();
+        /*$("#idSeccion02").hide();
+        $("#idSeccion03").hide();*/
     });
-    
+
+    $("#idSecUno").on("click", function(){
+        $("#idLanding").hide();
+        $("#idSeccion01").show();
+        /*$("#idSeccion02").hide();
+        $("#idSeccion03").hide();*/
+    });
+    /*
     $("#idSecDos").on("click", function(){
+        $("#idLanding").hide();
         $("#idSeccion01").hide();
         $("#idSeccion02").show();
         $("#idSeccion03").hide();
     });
-
+    */
 
     
 }
